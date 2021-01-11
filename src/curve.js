ea067bd2d6f61a6f05db6eb417a0fdfc2194fba2
BLAIN THOMAS p1601248 <thomas.blain@etu.univ-lyon1.fr>
2021-01-11 20:29:28 +0100

    correction des couleurs de la courbe + ajout de legendes + ajout d'une tooltip quand on survole une zone  curve.js

--- src/curve.js
+++ src/curve.js
@@ -1,8 +1,10 @@
 // ================================================
 // creer le graph qui decrit l'augmentation du nombre d'extinction au cours du temps
 export function drawCurve(data_decade, list_causes) {
+  var tooltip = d3.select("body").append("div").attr("class", "hidden tooltip");
+
   // set the dimensions and margins of the graph
-  var margin_c = { top: 70, right: 30, bottom: 100, left: 100 },
+  var margin_c = { top: 50, right: 30, bottom: 100, left: 100 },
     width_c = 1000 - margin_c.left - margin_c.right,
     height_c = 500 - margin_c.top - margin_c.bottom;
 
@@ -18,10 +20,12 @@ export function drawCurve(data_decade, list_causes) {
   svg_c
     .append("text")
     .attr("x", width_c / 2)
-    .attr("y", margin_c.top)
+    .attr("y", margin_c.top - 70)
     .attr("text-anchor", "middle")
     .style("text-decoration", "underline")
-    .text("Global number of species extincted over time");
+    .text(
+      "Evolution of the number of species extincted over time by causes of extinction"
+    );
 
   // Add X axis --> it is a date format
   var x = d3
@@ -39,7 +43,6 @@ export function drawCurve(data_decade, list_causes) {
     .call(d3.axisBottom(x));
 
   // Add Y axis
-  //TODO: changer les noms de x et y
   var y = d3
     .scaleLinear()
     .domain([
@@ -68,26 +71,8 @@ export function drawCurve(data_decade, list_causes) {
       return d[1][0].extinctionCauses[key];
     })(sumstat);
 
-  console.log("stackedData", stackedData);
-
-  // var color = d3
-  //   .scaleOrdinal()
-  //   .domain(mygroups)
-  //   .range([
-  //     "#e41a1c",
-  //     "#377eb8",
-  //     "#4daf4a",
-  //     "#984ea3",
-  //     "#ff7f00",
-  //     "#ffff33",
-  //     "#a65628",
-  //     "#f781bf",
-  //     "#999999"
-  //   ]);
-
   var color = d3.scaleOrdinal().domain(mygroups).range(d3.schemeSet2);
-  // Add the area
-  // TODO ajouter un titre et les descriptions des abscisses
+
   // Add the area
   svg_c
     .selectAll("mylayers")
@@ -101,6 +86,30 @@ export function drawCurve(data_decade, list_causes) {
       let name = d.key;
       return color(name);
     })
+    .on("mousemove", function (event, d) {
+      var indice = Math.floor(
+        (((event.x - margin_c.left) / width_c) * (2000 - 1820) + 0) / 10
+      );
+      var nb_exctinction = d[indice - 1].data[1][0].extinctionCauses[d.key];
+      tooltip
+        .classed("hidden", false)
+        .attr(
+          "style",
+          "left:" + (event.x + 15) + "px; top:" + (event.y - 15) + "px"
+        )
+        // Le tooltip contient la cause d'extinction représentée par la partie survolée
+        // et le nombre d'espèces disparues par cette cause.
+        .html(
+          "<div><b>Cause of extinction : </b>" +
+            d.key +
+            "<br /><b>Number of species extincted by this cause : </b>" +
+            nb_exctinction +
+            "</div>"
+        );
+    })
+    .on("mouseout", function (event, d) {
+      tooltip.classed("hidden", true);
+    })
     .attr(
       "d",
       d3
@@ -108,15 +117,8 @@ export function drawCurve(data_decade, list_causes) {
         .x(function (d) {
           return x(d.data[0]);
         })
-        .y0(function (d, key) {
-          if (d.data[0] < 1850) console.log(mygroups[key]);
-          if (
-            mygroups[key] ==
-            "Habitat destruction and predation from introduced Euglandina rosea"
-          )
-            console.log(x(d[0]));
-
-          return x(d[0]);
+        .y0(function (d) {
+          return y(d[0]);
         })
         .y1(function (d) {
           return y(d[1]);
@@ -135,8 +137,29 @@ export function drawCurve(data_decade, list_causes) {
   svg_c
     .append("text")
     .attr("text-anchor", "end")
-    .attr("x", -100)
-    .attr("y", -50)
-    .text("# of baby born")
+    .attr("x", -50)
+    .attr("y", -30)
+    .text("# of extinction")
     .attr("text-anchor", "start");
+
+  //////////
+  // HIGHLIGHT GROUP //
+  //////////
+
+  // What to do when one group is hovered
+  var highlight = function (d) {
+    console.log(d);
+    // reduce opacity of all groups
+    d3.selectAll(".myArea").style("opacity", 0.1);
+    // expect the one that is hovered
+    d3.select("." + d).style("opacity", 1);
+  };
+
+  // And when it is not hovered anymore
+  var noHighlight = function (d) {
+    d3.selectAll(".myArea").style("opacity", 1);
+  };
 }
+
+// sources:
+// - https://www.d3-graph-gallery.com/graph/stackedarea_template.html
